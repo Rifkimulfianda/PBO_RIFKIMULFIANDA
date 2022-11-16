bcbfd34cf9bc0d4926d79c2ec9e6c602d136d693
Rifkimulfianda <114200796+Rifkimulfianda@users.noreply.github.com>
2022-11-16 14:52:18 +0700

    Add files via upload

---
+++ rifki_120922/DataDiri.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rifki_120922;
+
+/**
+ *
+ * @author ACER
+ */
+public class DataDiri {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Rifki Mulfianda");
+        System.out.println("No BP   : 2101092067");
+        System.out.println("Prodi   : D3 Manajemen Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+        System.out.println("Alamat  : Jl.pagambiran ampalu NAN XX");
+
+    }
+
+}
